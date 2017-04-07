0857c63c36b1adcd58abf9edbdbc9b396d1ef8d7
Giancarlo Vera <gianverab@gmail.com>
2017-04-06 23:07:46 -0300

    Firebase as our backend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import firebase from 'firebase'
+
 import './App.css'
 
 class App extends Component {
+  handleAuth () {
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`))
+  }
+
   render () {
     return (
       <div className='App'>
         <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
           <h2>Spidergram</h2>
         </div>
         <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.handleAuth}>Login with Google</button>
         </p>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
+import firebase from 'firebase'
+
 import App from './App'
 import './index.css'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyAErjh2fdlciBm7UjK1K343W9YrmETuIhY',
+  authDomain: 'spidergram-c7cc8.firebaseapp.com',
+  databaseURL: 'https://spidergram-c7cc8.firebaseio.com',
+  projectId: 'spidergram-c7cc8',
+  storageBucket: 'spidergram-c7cc8.appspot.com',
+  messagingSenderId: '213334105146'
+})
 
 render(
   <App />,
