cdb595769cf6b6b2d52dcb84957421559b98051f
Giancarlo Vera <gianverab@gmail.com>
2017-04-08 14:10:57 -0300

    Upload files with Firebase storage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
 
+import FileUpload from './FileUpload'
 import './App.css'
 
 class App extends Component {
@@ -36,8 +37,9 @@ class App extends Component {
     if (this.state.user) {
       return (
         <div>
-          <img src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <img className='App-user' src={this.state.user.photoURL} alt={this.state.user.displayName} />
           <p>Hola {this.state.user.displayName}!</p>
+          <FileUpload />
           <button onClick={this.handleLogout}>Salir</button>
         </div>
       )
